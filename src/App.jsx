06f431bbc783ee392d0878fbdbaf87c22ec75668
nickcode-celle <nick.virion@gmail.com>
2026-09-10 19:13:29 +0200

    Make EMAEA the production entrypoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 import { Toaster } from "@/components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import ScrollToTop from './components/ScrollToTop';
-import Home from './pages/Home';
-import Conversation from './pages/Conversation';
 import Entity from './pages/Entity';
 import EmaeaGraphicTest from './pages/EmaeaGraphicTest';
 
 function AppRoutes(){
   return <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/conversation" element={<Conversation />} />
+    <Route path="/" element={<Entity />} />
     <Route path="/entity" element={<Entity />} />
     <Route path="/entity-graphics-test" element={<EmaeaGraphicTest />} />
+    <Route path="/conversation" element={<Navigate to="/" replace />} />
     <Route path="*" element={<PageNotFound />} />
   </Routes>;
 }
