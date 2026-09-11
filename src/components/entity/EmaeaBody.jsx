f43207380837d5c5d733fe5e655f9da3fca3b757
nickcode-celle <nick.virion@gmail.com>
2026-09-11 18:21:48 +0200

    Increase responsive EMÆÄ viewport height

--- src/components/entity/EmaeaBody.jsx
+++ src/components/entity/EmaeaBody.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import EmaeaRuntimeHost from './EmaeaRuntimeHost.jsx';
 
 export default function EmaeaBody({entityId}){
-  return <div className="relative mx-auto mb-8 h-[38vh] min-h-[300px] max-h-[480px] w-full overflow-hidden rounded-[28px] border border-white/[0.06] bg-[#1d1f22]" aria-label="EMÆÄ">
+  return <div className="relative mx-auto mb-8 h-[46vh] min-h-[360px] max-h-[560px] w-full overflow-hidden rounded-[28px] border border-emerald-300/10 bg-[#0b1514] shadow-[0_22px_80px_rgba(0,0,0,.35)] sm:h-[50vh] sm:min-h-[410px] lg:h-[54vh] lg:min-h-[450px] lg:max-h-[640px]" aria-label="EMÆÄ">
     <EmaeaRuntimeHost entityId={entityId} className="absolute inset-0"/>
   </div>;
 }
